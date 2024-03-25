00eb55e0e931b6887e667b200bf912a941a1cbed
Huamin Chen <hchen@redhat.com>
2024-03-25 14:28:51 -0400

    fix formatting
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- src/climatik-operator/k8s_utils.py
+++ src/climatik-operator/k8s_utils.py
@@ -9,19 +9,22 @@ def list_pods_in_deployment(namespace, deployment_name, debug=False):
 
     try:
         # Get the deployment object
-        deployment = api_instance.read_namespaced_deployment(deployment_name, namespace)
+        deployment = api_instance.read_namespaced_deployment(
+            deployment_name, namespace)
         # Get the selector for the deployment
         selector = deployment.spec.selector.match_labels
         # Create a label selector for the pods
-        label_selector = ",".join([f"{key}={value}" for key, value in selector.items()])
+        label_selector = ",".join(
+            [f"{key}={value}" for key, value in selector.items()])
         # Get the pods matching the label selector
-        pods = api_instance.list_namespaced_pod(namespace, label_selector=label_selector)
+        pods = api_instance.list_namespaced_pod(namespace, 
+                                                label_selector=label_selector)
         # Print the names of the pods
         if debug:
             print(f"Pods in deployment {deployment_name}:")
             for pod in pods.items:
                 print(pod.metadata.name)
-        
+
         return pods
 
     except Exception as e:
