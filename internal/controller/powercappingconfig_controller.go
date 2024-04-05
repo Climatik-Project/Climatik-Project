0f40f3ffb954347e82e180cdb5893c943634de05
Huamin Chen <hchen@redhat.com>
2024-04-05 16:27:11 -0400

    initial operator plumbing
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- internal/controller/powercappingconfig_controller.go
+++ internal/controller/powercappingconfig_controller.go
@@ -19,10 +19,12 @@ package controller
 import (
 	"context"
 
+	"github.com/go-logr/logr"
+	kedav1alpha1 "github.com/kedacore/keda/v2/apis/keda/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	powercappingv1alpha1 "github.com/Climatik-Project/Climatik-Project/api/v1alpha1"
 )
@@ -31,6 +33,7 @@ import (
 type PowerCappingConfigReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	Log    logr.Logger
 }
 
 //+kubebuilder:rbac:groups=powercapping.climatik-project.ai,resources=powercappingconfigs,verbs=get;list;watch;create;update;patch;delete
@@ -39,17 +42,52 @@ type PowerCappingConfigReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the PowerCappingConfig object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *PowerCappingConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := r.Log.WithValues("powercappingconfig", req.NamespacedName)
 
-	// TODO(user): your logic here
+	// Fetch the PowerCappingConfig instance
+	powerCappingConfig := &powercappingv1alpha1.PowerCappingConfig{}
+	err := r.Get(ctx, req.NamespacedName, powerCappingConfig)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("PowerCappingConfig resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get PowerCappingConfig")
+		return ctrl.Result{}, err
+	}
+
+	// Retrieve the power capping configuration from the custom resource
+	powerCapLimit := powerCappingConfig.Spec.PowerCapLimit
+	scaledObjectRefs := powerCappingConfig.Spec.ScaledObjectRefs
+
+	// Iterate over the ScaledObjectRefs and update the KEDA ScaledObjects
+	for _, scaledObjectRef := range scaledObjectRefs {
+		scaledObject := &kedav1alpha1.ScaledObject{}
+		err := r.Get(ctx, client.ObjectKey{Namespace: req.Namespace, Name: scaledObjectRef.Metadata.Name}, scaledObject)
+		if err != nil {
+			if errors.IsNotFound(err) {
+				log.Info("ScaledObject resource not found. Ignoring since object must be deleted")
+				continue
+			}
+			log.Error(err, "Failed to get ScaledObject")
+			return ctrl.Result{}, err
+		}
+
+		// Update the ScaledObject with the power capping configuration
+		maxReplicas := int32(calculateMaxReplicas(powerCapLimit))
+		scaledObject.Spec.MaxReplicaCount = &maxReplicas
+
+		// Update the ScaledObject in the Kubernetes cluster
+		err = r.Update(ctx, scaledObject)
+		if err != nil {
+			log.Error(err, "Failed to update ScaledObject")
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Monitor power usage
+	go r.monitorPowerUsage(ctx, powerCappingConfig)
 
 	return ctrl.Result{}, nil
 }
