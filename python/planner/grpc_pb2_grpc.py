13aae1b649c424ef1586b7de376d762802b9f7d8
Huamin Chen <hchen@redhat.com>
2024-04-09 15:31:44 -0400

    update python grpc server
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- python/planner/grpc_pb2_grpc.py
+++ python/planner/grpc_pb2_grpc.py
@@ -15,10 +15,12 @@ class PlannerStub(object):
             channel: A grpc.Channel.
         """
         self.CalculateOptimalReplicas = channel.unary_unary(
-                '/planner.Planner/CalculateOptimalReplicas',
-                request_serializer=grpc__pb2.CalculateOptimalReplicasRequest.SerializeToString,
-                response_deserializer=grpc__pb2.CalculateOptimalReplicasResponse.FromString,
-                )
+            '/planner.Planner/CalculateOptimalReplicas',
+            request_serializer=grpc__pb2.CalculateOptimalReplicasRequest.
+            SerializeToString,
+            response_deserializer=grpc__pb2.CalculateOptimalReplicasResponse.
+            FromString,
+        )
 
 
 class PlannerServicer(object):
@@ -33,34 +35,38 @@ class PlannerServicer(object):
 
 def add_PlannerServicer_to_server(servicer, server):
     rpc_method_handlers = {
-            'CalculateOptimalReplicas': grpc.unary_unary_rpc_method_handler(
-                    servicer.CalculateOptimalReplicas,
-                    request_deserializer=grpc__pb2.CalculateOptimalReplicasRequest.FromString,
-                    response_serializer=grpc__pb2.CalculateOptimalReplicasResponse.SerializeToString,
-            ),
+        'CalculateOptimalReplicas':
+        grpc.unary_unary_rpc_method_handler(
+            servicer.CalculateOptimalReplicas,
+            request_deserializer=grpc__pb2.CalculateOptimalReplicasRequest.
+            FromString,
+            response_serializer=grpc__pb2.CalculateOptimalReplicasResponse.
+            SerializeToString,
+        ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
-            'planner.Planner', rpc_method_handlers)
-    server.add_generic_rpc_handlers((generic_handler,))
+        'planner.Planner', rpc_method_handlers)
+    server.add_generic_rpc_handlers((generic_handler, ))
 
 
- # This class is part of an EXPERIMENTAL API.
+# This class is part of an EXPERIMENTAL API.
 class Planner(object):
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
     def CalculateOptimalReplicas(request,
-            target,
-            options=(),
-            channel_credentials=None,
-            call_credentials=None,
-            insecure=False,
-            compression=None,
-            wait_for_ready=None,
-            timeout=None,
-            metadata=None):
-        return grpc.experimental.unary_unary(request, target, '/planner.Planner/CalculateOptimalReplicas',
+                                 target,
+                                 options=(),
+                                 channel_credentials=None,
+                                 call_credentials=None,
+                                 insecure=False,
+                                 compression=None,
+                                 wait_for_ready=None,
+                                 timeout=None,
+                                 metadata=None):
+        return grpc.experimental.unary_unary(
+            request, target, '/planner.Planner/CalculateOptimalReplicas',
             grpc__pb2.CalculateOptimalReplicasRequest.SerializeToString,
-            grpc__pb2.CalculateOptimalReplicasResponse.FromString,
-            options, channel_credentials,
-            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+            grpc__pb2.CalculateOptimalReplicasResponse.FromString, options,
+            channel_credentials, insecure, call_credentials, compression,
+            wait_for_ready, timeout, metadata)
