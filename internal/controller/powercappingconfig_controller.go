742947e7363f8c489b831a3aa799b2f11b30f803
Huamin Chen <hchen@redhat.com>
2024-06-28 12:21:27 -0400

    refactor
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- internal/controller/powercappingconfig_controller.go
+++ internal/controller/powercappingconfig_controller.go
@@ -16,8 +16,12 @@ limitations under the License.
 package controller
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -32,13 +36,17 @@ import (
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
+
+	prom_api "github.com/prometheus/client_golang/api"
+	prom_v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
 )
 
 const (
-	labelKey            = "powercapping.climatik.io"
-	resourceName        = "nvidia.com/gpu"
-	defaultPowerCapHigh = 90
-
+	labelKey              = "powercapping.climatik.io"
+	Prom_URL              = "http://prometheus:9090"
+	AlertmanagerURL       = "http://alertmanager:9093/api/v1/alerts"
+	defaultPowerCapHigh   = 90
 	defaultPowerCapMedium = 80
 	defaultPowerCapLow    = 50
 )
@@ -46,9 +54,10 @@ const (
 // PowerCappingConfigReconciler reconciles a PowerCappingConfig object
 type PowerCappingConfigReconciler struct {
 	client.Client
-	Scheme      *runtime.Scheme
-	Log         logr.Logger
-	PodInformer cache.SharedIndexInformer
+	Scheme           *runtime.Scheme
+	Log              logr.Logger
+	PodInformer      cache.SharedIndexInformer
+	PrometheusClient prom_v1.API
 }
 
 //+kubebuilder:rbac:groups=powercapping.climatik-project.ai,resources=powercappingconfigs,verbs=get;list;watch;create;update;patch;delete
@@ -72,9 +81,6 @@ func (r *PowerCappingConfigReconciler) Reconcile(ctx context.Context, req ctrl.R
 		return ctrl.Result{}, err
 	}
 
-	// Monitor power usage
-
-	go r.monitorPowerUsage(ctx, powerCappingConfig)
 	return ctrl.Result{}, nil
 }
 
@@ -113,10 +119,41 @@ func (r *PowerCappingConfigReconciler) SetupWithManager(mgr ctrl.Manager) error 
 	if ok := cache.WaitForCacheSync(context.Background().Done(), r.PodInformer.HasSynced); !ok {
 		return fmt.Errorf("failed to wait for caches to sync")
 	}
+	promClient, err := prom_api.NewClient(prom_api.Config{
+		Address: Prom_URL,
+	})
+	if err != nil {
+		return err
+	}
+	r.PrometheusClient = prom_v1.NewAPI(promClient)
 
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&powercappingv1alpha1.PowerCappingConfig{}).
 		Complete(r)
+}
+
+func (r *PowerCappingConfigReconciler) getKeplerMetrics(ctx context.Context, podName, label string) (float64, string, error) {
+	query := fmt.Sprintf(`kepler_container_%s_joules_total{pod='%s'}`, podName, label)
+	result, warnings, err := r.PrometheusClient.Query(ctx, query, time.Now())
+	if err != nil {
+		return 0, "", err
+	}
+	if len(warnings) > 0 {
+		r.Log.Info("Prometheus query warnings", "warnings", warnings)
+	}
+	if result.Type() == model.ValVector {
+		vector := result.(model.Vector)
+		if len(vector) > 0 {
+			for _, sample := range vector {
+				for labelName, labelValue := range sample.Metric {
+					if string(labelName) == label {
+						return float64(sample.Value), string(labelValue), nil
+					}
+				}
+			}
+		}
+	}
+	return 0, "", fmt.Errorf("no data with gpu label returned from Prometheus query")
 }
 
 func (r *PowerCappingConfigReconciler) handlePodAdd(obj interface{}) {
@@ -128,19 +165,8 @@ func (r *PowerCappingConfigReconciler) handlePodAdd(obj interface{}) {
 	// Check if the pod has the powercapping label
 	powerCapLabel := pod.Labels[labelKey]
 	if powerCapLabel != "" {
-		// Check if the pod claims GPU resources
-		containsGPU := false
-		for _, container := range pod.Spec.Containers {
-			if _, ok := container.Resources.Limits[resourceName]; ok {
-				containsGPU = true
-				break
-			}
-		}
-
-		if containsGPU {
-			// Handle the pod with GPU
-			go r.handlePodWithGPU(pod, powerCapLabel)
-		}
+		// add the pod to the list of pods to monitor
+		r.getPodDevices(pod, powerCapLabel)
 	}
 }
 
@@ -148,14 +174,20 @@ func (r *PowerCappingConfigReconciler) handlePodDelete(obj interface{}) {
 	// Handle pod deletion if needed
 }
 
-func (r *PowerCappingConfigReconciler) handlePodWithGPU(pod *corev1.Pod, powerCapLabel string) {
-	// Get device info from the device plugin
-	deviceInfo := r.getDeviceInfo(pod)
+func (r *PowerCappingConfigReconciler) getPodDevices(pod *corev1.Pod, powerCapLabel string) {
+	devices := make(map[string]string, 2)
+	ctx := context.Background()
+	powerConsumption := float64(0)
+	for _, v := range []string{"package", "gpu"} {
+		powerConsumptionDevice, label, err := r.getKeplerMetrics(ctx, pod.Name, v)
+		if err != nil {
+			r.Log.Error(err, "Failed to get Kepler metrics")
+			return
+		}
+		devices[v] = label
+		powerConsumption += powerConsumptionDevice
+	}
 
-	// Monitor GPU power consumption and temperature
-	powerConsumption, temperature := r.monitorGPU(deviceInfo)
-
-	// Determine power cap percentage based on label
 	powerCapPercentage := 0
 	switch powerCapLabel {
 	case "high":
@@ -169,15 +201,71 @@ func (r *PowerCappingConfigReconciler) handlePodWithGPU(pod *corev1.Pod, powerCa
 		return
 	}
 
-	// Calculate power cap value based on observed peak power consumption
 	powerCap := int(float64(powerConsumption) * float64(powerCapPercentage) / 100)
+	r.createPrometheusAlert(pod, powerCap, devices)
+}
 
-	// Set GPU power cap
-	err := r.setGPUPowerCap(deviceInfo, powerCap)
-	if err != nil {
-		r.Log.Error(err, "Failed to set GPU power cap")
-		return
+func (r *PowerCappingConfigReconciler) createPrometheusAlert(pod *corev1.Pod, powerCap int, deviceLabels map[string]string) error {
+	device := ""
+	for k, label := range deviceLabels {
+		device += k + ":" + label + ","
+	}
+	alert := fmt.Sprintf(`ALERT PowerCappingAlert
+    IF rate(kepler_container_joules_total{pod='%s'}) > %d
+    FOR 5m
+    LABELS { severity="critical" }
+    ANNOTATIONS {
+        summary = "Power capping alert for pod %s",
+        description = "The pod is exceeding the power cap of %d watts."
+		device = "%s"
+    }`, pod.Name, powerCap, pod.Name, powerCap, device)
+
+	r.Log.Info("Creating Prometheus alert", "alert", alert)
+	err := r.sendAlertToPrometheus(alert)
+
+	return err
+}
+
+type PrometheusAlert struct {
+	Labels      map[string]string `json:"labels"`
+	Annotations map[string]string `json:"annotations"`
+	StartsAt    time.Time         `json:"startsAt"`
+}
+
+func (r *PowerCappingConfigReconciler) sendAlertToPrometheus(alert string) error {
+	alertStruct := PrometheusAlert{
+		Labels: map[string]string{
+			"alertname": "PowerCappingAlert",
+			"severity":  "critical",
+		},
+		Annotations: map[string]string{
+			"summary":     "Power capping alert",
+			"description": alert,
+		},
+		StartsAt: time.Now(),
 	}
 
-	r.Log.Info("Power capping applied", "pod", pod.Name, "label", powerCapLabel, "powerCap", powerCap, "temperature", temperature)
+	alertBody, err := json.Marshal([]PrometheusAlert{alertStruct})
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", AlertmanagerURL, bytes.NewBuffer(alertBody))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to send alert to Prometheus Alertmanager: %s", resp.Status)
+	}
+
+	return nil
 }
