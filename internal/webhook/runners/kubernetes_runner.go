7d85ce37bec19a92a4f960b64a7ee3ca9463a128
Huamin Chen <hchen@redhat.com>
2024-08-01 14:17:43 -0400

    chore: create cpu freq tuner k8s job
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- internal/webhook/runners/kubernetes_runner.go
+++ internal/webhook/runners/kubernetes_runner.go
@@ -1,16 +1,25 @@
 package runners
 
 import (
+	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
+	"text/template"
 
 	batchv1 "k8s.io/api/batch/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
+
+// JobConfig holds the configuration for the Job
+type JobConfig struct {
+	JobName    string
+	Namespace  string
+	Percentage string
+}
 
 type KubernetesRunner struct {
 	JobManifestPath string
@@ -35,19 +44,51 @@ func (r *KubernetesRunner) Run() error {
 		return fmt.Errorf("failed to read job manifest: %v", err)
 	}
 
-	// Create a Job object from the manifest
-	job := &batchv1.Job{}
-	if err := json.Unmarshal(manifest, job); err != nil {
-		return fmt.Errorf("failed to unmarshal job manifest: %v", err)
+	// Create a new JobConfig and set the percentage value
+	jobConfig := JobConfig{
+		Percentage: "80", //FIXME Set the percentage from alert here
 	}
 
-	// Create the Job in the Kubernetes cluster
-	jobClient := clientset.BatchV1().Jobs("default")
-	createdJob, err := jobClient.Create(context.TODO(), job, metav1.CreateOptions{})
+	// Parse the template
+	tmpl, err := template.New("job").Parse(string(manifest))
 	if err != nil {
-		return fmt.Errorf("failed to create Kubernetes job: %v", err)
+		panic(err)
 	}
 
-	fmt.Printf("Job %s created successfully\n", createdJob.Name)
+	// Execute the template with the JobConfig data
+	var jobBuffer bytes.Buffer
+	if err := tmpl.Execute(&jobBuffer, jobConfig); err != nil {
+		panic(err)
+	}
+
+	// Decode the Job from the YAML
+	decoder := yaml.NewYAMLOrJSONDecoder(&jobBuffer, 1024)
+	var job batchv1.Job
+	if err := decoder.Decode(&job); err != nil {
+		panic(err)
+	}
+
+	// Delete the existing job if it exists
+	jobConfig.JobName = job.Name
+	jobConfig.Namespace = job.Namespace
+	jobsClient := clientset.BatchV1().Jobs(jobConfig.Namespace)
+	err = jobsClient.Delete(context.TODO(), jobConfig.JobName, metav1.DeleteOptions{
+		PropagationPolicy: func() *metav1.DeletionPropagation {
+			policy := metav1.DeletePropagationForeground
+			return &policy
+		}(),
+	})
+	if err != nil {
+		fmt.Printf("Job %s does not exist or could not be deleted: %v\n", jobConfig.JobName, err)
+	} else {
+		fmt.Printf("Deleted existing job %s\n", jobConfig.JobName)
+	}
+	// Create the Job in the Kubernetes cluster
+	result, err := jobsClient.Create(context.TODO(), &job, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Job %s status %v", jobConfig.JobName, result)
 	return nil
 }
