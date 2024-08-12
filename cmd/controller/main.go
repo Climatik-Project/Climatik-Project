a32649584c1405f434c9e583b271e92b41f5097e
Huamin Chen <hchen@redhat.com>
2024-08-12 14:19:46 -0400

    chore: convert group name to climatik-project.io in all instances
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -110,7 +110,7 @@ func main() {
 		WebhookServer: webhookServer,
 		// HealthProbeBindAddress: probeAddr,
 		LeaderElection:   enableLeaderElection,
-		LeaderElectionID: "cf3a8ad7.climatik-project.ai",
+		LeaderElectionID: "cf3a8ad7.climatik-project.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
